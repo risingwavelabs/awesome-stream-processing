cf6060007a33ed7752786ee597a65b5c651a2829
tinytimcodes <tjc.choy@gmail.com>
2025-07-04 02:14:28 -0400

    run w o postgres

--- 02-simple-demos/capital_markets/market_data_enrichment/bootstrap.sql
+++ 02-simple-demos/capital_markets/market_data_enrichment/bootstrap.sql
@@ -1,5 +1,5 @@
 -- Base tables
-CREATE TABLE raw_market_data (
+CREATE TABLE IF NOT EXISTS raw_market_data (
   asset_id INT,
   timestamp TIMESTAMPTZ,
   price NUMERIC,
@@ -8,7 +8,7 @@ CREATE TABLE raw_market_data (
   ask_price NUMERIC
 );
 
-CREATE TABLE enrichment_data (
+CREATE TABLE IF NOT EXISTS enrichment_data (
   asset_id INT,
   sector VARCHAR,
   historical_volatility NUMERIC,
@@ -17,24 +17,36 @@ CREATE TABLE enrichment_data (
   timestamp TIMESTAMPTZ
 );
 
--- Materialized Views
-CREATE MATERIALIZED VIEW avg_price_bid_ask_spread AS
-SELECT
+-- Materialized Views (live entirely inside RisingWave)
+CREATE MATERIALIZED VIEW IF NOT EXISTS avg_price_bid_ask_spread AS
+  SELECT
     asset_id,
-    ROUND(AVG(price) OVER (PARTITION BY asset_id ORDER BY timestamp RANGE INTERVAL '5 MINUTES' PRECEDING), 2) AS average_price,
-    ROUND(AVG(ask_price - bid_price) OVER (PARTITION BY asset_id ORDER BY timestamp RANGE INTERVAL '5 MINUTES' PRECEDING), 2) AS bid_ask_spread,
+    ROUND(AVG(price)  OVER (
+      PARTITION BY asset_id
+      ORDER BY timestamp
+      RANGE INTERVAL '5 MINUTES' PRECEDING
+    ),2) AS average_price,
+    ROUND(AVG(ask_price - bid_price) OVER (
+      PARTITION BY asset_id
+      ORDER BY timestamp
+      RANGE INTERVAL '5 MINUTES' PRECEDING
+    ),2) AS bid_ask_spread,
     timestamp
-FROM raw_market_data;
+  FROM raw_market_data;
 
-CREATE MATERIALIZED VIEW rolling_volatility AS
-SELECT
+CREATE MATERIALIZED VIEW IF NOT EXISTS rolling_volatility AS
+  SELECT
     asset_id,
-    ROUND(stddev_samp(price) OVER (PARTITION BY asset_id ORDER BY timestamp RANGE INTERVAL '15 MINUTES' PRECEDING), 2) AS rolling_volatility,
+    ROUND(stddev_samp(price) OVER (
+      PARTITION BY asset_id
+      ORDER BY timestamp
+      RANGE INTERVAL '15 MINUTES' PRECEDING
+    ),2) AS rolling_volatility,
     timestamp
-FROM raw_market_data;
+  FROM raw_market_data;
 
-CREATE MATERIALIZED VIEW enriched_market_data AS
-SELECT
+CREATE MATERIALIZED VIEW IF NOT EXISTS enriched_market_data AS
+  SELECT
     rmd.asset_id,
     ap.average_price,
     (rmd.price - ap.average_price) / ap.average_price * 100 AS price_change,
@@ -43,57 +55,13 @@ SELECT
     ed.sector_performance,
     ed.sentiment_score,
     rmd.timestamp
-FROM
-    raw_market_data AS rmd
-JOIN 
-    avg_price_bid_ask_spread AS ap ON rmd.asset_id = ap.asset_id
+  FROM raw_market_data AS rmd
+  JOIN avg_price_bid_ask_spread AS ap
+    ON rmd.asset_id = ap.asset_id
     AND rmd.timestamp BETWEEN ap.timestamp - INTERVAL '2 seconds' AND ap.timestamp + INTERVAL '2 seconds'
-JOIN 
-    rolling_volatility AS rv ON rmd.asset_id = rv.asset_id
+  JOIN rolling_volatility AS rv
+    ON rmd.asset_id = rv.asset_id
     AND rmd.timestamp BETWEEN rv.timestamp - INTERVAL '2 seconds' AND rv.timestamp + INTERVAL '2 seconds'
-JOIN 
-    enrichment_data AS ed ON rmd.asset_id = ed.asset_id
+  JOIN enrichment_data AS ed
+    ON rmd.asset_id = ed.asset_id
     AND rmd.timestamp BETWEEN ed.timestamp - INTERVAL '2 seconds' AND ed.timestamp + INTERVAL '2 seconds';
-
--- Sinks
-CREATE SINK sink_avg_price
-FROM avg_price_bid_ask_spread
-WITH (
-  connector = 'postgres',
-  type = 'append-only',
-  force_append_only = 'true',
-  host = 'postgres',
-  port = 5432,
-  user = 'pguser',
-  password = 'pgpass',
-  database = 'pgdb',
-  table = 'avg_price_sink'
-);
-
-CREATE SINK sink_rolling_volatility
-FROM rolling_volatility
-WITH (
-  connector = 'postgres',
-  type = 'append-only',
-  force_append_only = 'true',
-  host = 'postgres',
-  port = 5432,
-  user = 'pguser',
-  password = 'pgpass',
-  database = 'pgdb',
-  table = 'rolling_volatility_sink'
-);
-
-CREATE SINK sink_enriched
-FROM enriched_market_data
-WITH (
-  connector = 'postgres',
-  type = 'append-only',
-  force_append_only = 'true',
-  host = 'postgres',
-  port = 5432,
-  user = 'pguser',
-  password = 'pgpass',
-  database = 'pgdb',
-  table = 'enriched_market_data_sink'
-);
