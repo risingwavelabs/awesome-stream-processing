33e2b0c6b81c01e53e3d9e0c2e9ae585348ea520
tinytimcodes <tjc.choy@gmail.com>
2025-07-10 03:13:00 -0400

    server diverted to kafka instead of localhost

--- 02-simple-demos/capital_markets/market_data_enrichment/data_generator.py
+++ 02-simple-demos/capital_markets/market_data_enrichment/data_generator.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 import time
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers=['kafka:9092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
