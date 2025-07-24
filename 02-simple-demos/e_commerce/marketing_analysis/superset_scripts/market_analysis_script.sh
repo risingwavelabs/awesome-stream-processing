9d3fc7369dea01a2d38eeb2aea540cf1b4f6e67d
tinytimcodes <tjc.choy@gmail.com>
2025-07-24 05:31:37 -0400

    added time grain to A/B

--- 02-simple-demos/e_commerce/marketing_analysis/superset_scripts/market_analysis_script.sh
+++ 02-simple-demos/e_commerce/marketing_analysis/superset_scripts/market_analysis_script.sh
@@ -153,28 +153,19 @@ CH_FILTER="q=$(jq -n --arg name "$CH_NAME" '{filters:[{col:"slice_name",opr:"eq"
 CH_PARAMS=$(jq -n --argjson ds_id "$AB_TEST_RESULTS_DS_ID" '{
   viz_type:"bar",
   datasource:"\($ds_id)__table",
-  time_range:"Last 1 hour",
+  granularity_sqla:"window_start",
+  time_range:"No filter",
   metrics: [
     {
       label: "Average Conversion Rate",
       expressionType: "SQL",
       sqlExpression: "AVG(conversion_rate)"
-    },
-    {
-      label: "Total Conversions", 
-      expressionType: "SQL",
-      sqlExpression: "SUM(conversions)"
-    },
-    {
-      label: "Total Revenue",
-      expressionType: "SQL", 
-      sqlExpression: "SUM(revenue)"
     }
   ],
   groupby:["variant_name"],
+  time_grain_sqla:"PT1H",
   row_limit:10000,
-  show_legend:true,
-  adhoc_filters: []
+  show_legend:true
 }')
 CH_PAYLOAD=$(jq -n --arg name "$CH_NAME" --argjson ds_id "$AB_TEST_RESULTS_DS_ID" --argjson params "$CH_PARAMS" \
   '{slice_name:$name,viz_type:"bar",datasource_id:$ds_id,datasource_type:"table",params:($params|tostring),owners:[1]}')
